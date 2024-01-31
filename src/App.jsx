4efb3ef09b4a516e9cbee459c33a5688bfd23281
majkeloess <msaturczak@student.agh.edu.pl>
2024-01-31 19:07:40 +0100

    v0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
 
       return (
             <div className="p-0 bg-slate-500">
-                  <h1 className="p-0 m-0">weather App</h1>
+                  <h1>weather App</h1>
             </div>
       );
 }
